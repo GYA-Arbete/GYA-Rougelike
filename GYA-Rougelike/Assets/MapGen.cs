cfcd9e0bdffac279f4188f7cc93195d043ad4bdf
Robotwars1 <magnus.carlsson4@gmail.com>
2023-09-11 16:04:03 +0200

    Kinda working PathGen

--- GYA-Rougelike/Assets/MapGen.cs
+++ GYA-Rougelike/Assets/MapGen.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,10 +16,17 @@ public class MapGen : MonoBehaviour
     public GameObject MapPrefab;
 
     [Space]
+    [Header("Map Line Bullshit")]
+
+    public Transform LinePrefabParent;
+    public Transform LinePrefab;
+
+    [Space]
     [Header("Arrays")]
 
     public Transform[] children;
     public Transform[] Clones;
+    public Transform[] Lines;
 
     // Start is called before the first frame update
     void Start()
@@ -43,18 +51,41 @@ public class MapGen : MonoBehaviour
     {
         // Put all clones + parent into Array
         Clones = MapPrefabParent.GetComponentsInChildren<Transform>();
-        for (int i = 0; i < Clones.Length; i++)
+
+        
+        for (int i = 2; i < Clones.Length; i++)
         {
-            // Ignore the first 2, eg Parent and Template
-            if (i > 1)
+            if (Clones[i] != null)
             {
                 // Destroy object
                 Destroy(Clones[i].gameObject);
             }
         }
 
+        /*
+        for (int i = 2; i < Lines.Length; i++)
+        {
+            if (Lines[i] != null)
+            {
+                // Destroy object
+                Destroy(Lines[i].gameObject);
+            }
+        }
+
+        /*
         // Clear array
-        Clones = null;
+        if (Clones.Length > 2)
+        {
+            Array.Clear(Clones, 2, Clones.Length - 2);
+        }
+
+        if (Lines.Length > 2)
+        {
+            Array.Clear(Lines, 2, Lines.Length - 2);
+        }
+        */
+        //Clones = null;
+        //Lines = null;
     }
 
     // Function for generating rooms on map
@@ -64,7 +95,8 @@ public class MapGen : MonoBehaviour
 
         // Slumpa mängden element per kolumn
         // Upper bounds is not inclusive, eg Next(1, 4) == 1, 2 or 3
-        int[] RoomCount = { rand.Next(1, 4), rand.Next(1, 4), rand.Next(1, 4), rand.Next(1, 4), rand.Next(1, 4) };
+        //int[] RoomCount = { rand.Next(1, 4), rand.Next(1, 4), rand.Next(1, 4), rand.Next(1, 4), rand.Next(1, 4) };
+        int[] RoomCount = { 2, 2, 2, 2, 2 };
 
         // Bestäm vilken / vilka spawnpoints som ska bli rum
         int[] SpawnPoint = new int[children.Length];
@@ -79,6 +111,7 @@ public class MapGen : MonoBehaviour
         {
             switch (RoomCount[i - 1])
             {
+                /*
                 case 1:
                     int temp1 = rand.Next(0, 3);
                     switch (temp1)
@@ -100,6 +133,7 @@ public class MapGen : MonoBehaviour
                             break;
                     }
                     break;
+                */
                 case 2:
                     int temp2 = rand.Next(0, 3);
                     switch (temp2)
@@ -121,11 +155,13 @@ public class MapGen : MonoBehaviour
                             break;
                     }
                     break;
+                /*
                 case 3:
                     SpawnPoint[i * 3 - 1] = 1;
                     SpawnPoint[i * 3] = 1;
                     SpawnPoint[i * 3 + 1] = 1;
                     break;
+                */
             }
         }
 
@@ -138,11 +174,55 @@ public class MapGen : MonoBehaviour
                 Instantiate(MapPrefab, new Vector3(children[i].position.x, children[i].position.y, children[i].position.z), new Quaternion(0, 0, 0, 0), MapPrefabParent);
             }
         }
+
+        // Put all clones + parent into Array
+        Clones = MapPrefabParent.GetComponentsInChildren<Transform>();
     }
 
     // Function for generating paths between rooms
     void GeneratePaths()
     {
+        //
+        int LineCount = 2;
 
+        for (int i = 2; i < Clones.Length; i++)
+        {
+            for (int j = 2; j < Clones.Length; j++)
+            {
+                // Så att den inte kollar avstånd mellan objekt n och objekt n
+                if (i != j)
+                {
+                    // Calc delta x & delta y
+                    float dX = System.Math.Abs(Clones[i].position.x - Clones[j].position.x);
+                    float dY = System.Math.Abs(Clones[i].position.y - Clones[j].position.y);
+
+                    if (dX < System.Math.Sqrt(2) && dY < System.Math.Sqrt(2))
+                    {
+                        // Clear array
+                        Lines = null;
+
+                        Instantiate(LinePrefab, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), LinePrefabParent);
+
+                        // Put all clones + parent into Array
+                        Lines = LinePrefabParent.GetComponentsInChildren<Transform>();
+
+                        LineRenderer LineRend = Lines[LineCount].GetComponent<LineRenderer>();
+
+                        LineRend.SetPosition(0, new Vector3(Clones[i].position.x, Clones[i].position.y, 0));
+
+                        if (j > i && dX > 0)
+                        {
+                            LineRend.SetPosition(1, new Vector3(Clones[i].position.x + dX, Clones[i].position.y + dY, 0));
+                        }
+                        else if (j < i && dX > 0)
+                        {
+                            LineRend.SetPosition(1, new Vector3(Clones[i].position.x - dX, Clones[i].position.y - dY, 0));
+                        }
+
+                        LineCount++;
+                    }
+                }
+            }
+        }
     }
 }
