90e487e6f8e72f7d26250a47674d428ce5574f91
Robotwars1 <magnus.carlsson4@gmail.com>
2023-09-22 09:05:57 +0200

    Feat. Somewhat working CardSpawning

--- GYA-Rougelike/Assets/Scripts/PlayerCards.cs
+++ GYA-Rougelike/Assets/Scripts/PlayerCards.cs
@@ -6,33 +6,48 @@ public class PlayerCards : MonoBehaviour
 {
     public string JsonString;
 
+    public Transform CardSpawner;
+    public Transform[] CardSpawnPoints;
+
+    public Transform CardsParent;
+
+    public GameObject CardPrefab;
+
     public CardList cardList;
 
     [System.Serializable]
     public class CardList
     {
-
         public List<Cards> cardList;
     }
 
     [System.Serializable]
     public class Cards
     {
-        public int energy { get; set; }
-        public int damage { get; set; }
-        public int defence { get; set; }
-        public int cooldown { get; set; }
+        public int Energy { get; set; }
+        public int Damage { get; set; }
+        public int Defence { get; set; }
+        public int Cooldown { get; set; }
     }
 
     // Start is called before the first frame update
     void Start()
     {
         cardList = JsonUtility.FromJson<CardList>(JsonString);
+
+        // Put parent + children into array
+        CardSpawnPoints = GetComponentsInChildren<Transform>();
+
+        SpawnCards();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnCards()
     {
-        
+        for (int i = 0; i < CardSpawnPoints.Length; i++)
+        {
+            // https://docs.unity3d.com/ScriptReference/Object.Instantiate.html
+            // Instantiate(Object original, Vector3 position, Quaternion rotation, Transform parent);
+            Instantiate(CardPrefab, new Vector3(CardSpawnPoints[i].position.x, CardSpawnPoints[i].position.y, CardSpawnPoints[i].position.z), new Quaternion(0, 0, 0, 0), CardsParent);
+        }
     }
 }
