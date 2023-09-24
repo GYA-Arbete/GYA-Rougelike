5a2021f04b163a52993132bc1b1285b2ac4749fe
Robotwars1 <magnus.carlsson4@gmail.com>
2023-09-24 11:11:34 +0200

    Fix. Removed unneeded function in DragDropCards.cs

--- GYA-Rougelike/Assets/Scripts/DragDropCard.cs
+++ GYA-Rougelike/Assets/Scripts/DragDropCard.cs
@@ -10,17 +10,15 @@ public class DragDropCard : MonoBehaviour
     {
         isDragging = true;
     }
+
     public void OnMouseUp()
     {
         isDragging = false;
     }
+
     public void OnMouseDrag()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         transform.Translate(mousePosition);
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("collision " + other.name);
     }
 }
