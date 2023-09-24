f682eed4bf05b9fd9bfbedd6d5e5ca1392f40bc9
Robotwars1 <magnus.carlsson4@gmail.com>
2023-09-24 11:07:15 +0200

    Feat. Drag and drop cards

---
+++ GYA-Rougelike/Assets/Scripts/DragDropCard.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDropCard : MonoBehaviour
+{
+    public bool isDragging;
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+    public void OnMouseDrag()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        transform.Translate(mousePosition);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collision " + other.name);
+    }
+}
