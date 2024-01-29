2d94e6b42d32b7ad80a5ee5b7a0500dcb26b8dae
Robotwars1 <magnus.carlsson4@gmail.com>
2024-01-29 13:43:51 +0100

    Feat. More MainMenu work
    #47

--- GYA-Rougelike/Assets/Scripts/MainMenu/MainMenu.cs
+++ GYA-Rougelike/Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,20 +5,26 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     [Header("Main Menu Items")]
-
     public Button PlayGameButton;
+    public Button OptionsButton;
     public Button QuitGameButton;
 
     // Start is called before the first frame update
     void Start()
     {
         PlayGameButton.onClick.AddListener(StartGame);
+        OptionsButton.onClick.AddListener(OpenOptions);
         QuitGameButton.onClick.AddListener(QuitGame);
     }
 
     void StartGame()
     {
         SceneManager.LoadScene("MapGenerator");
+    }
+
+    void OpenOptions()
+    {
+        // Do stuff
     }
 
     void QuitGame()
